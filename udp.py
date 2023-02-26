ca136d350f7e6df597372468d4d3e82f68c63171
unknown <mahikgot@gmail.com>
2023-02-26 22:14:14 +0800

    fix socket not releasing on ctrl+c

--- udp.py
+++ udp.py
@@ -3,7 +3,7 @@ from config import UDP_IP, UDP_PORT
 
 def udp_init():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((UDP_IP, UDP_PORT))
 
     return sock
