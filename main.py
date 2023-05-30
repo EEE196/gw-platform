a2178f12f6f2eed7e8234e1fb605ce4c42ec8ae8
unknown <mahikgot@gmail.com>
2023-05-30 16:21:11 +0800

    get data only

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from udp import udp_init
 
-
 if __name__ == "__main__":
     sock = udp_init()
-
+    key = '00000000000000000000000000000000'
+    dev_addr = 'DEADBEEF'
 
     print("Waiting to receive data...")
     while True:
         data, addr = sock.recvfrom(512)
-        print("received:", data, " from", addr)
 
-
-
-        
+        data = data[12:].decode('utf8')
+        try:
+            data = json.loads(data)
+            data = data['rxpk'][0]['data']
+            print(data)
+        except:
+            pass       
