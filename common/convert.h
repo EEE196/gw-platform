a3b6cc98b272cacef1c6f9f69e666175f88f67d2
unknown <mahikgot@gmail.com>
2023-06-05 16:47:59 +0800

    refactor

---
+++ common/convert.h
@@ -0,0 +1,29 @@
+
+typedef struct{
+    // GGA - Global Positioning System Fixed Data
+    float nmea_longitude;
+    float nmea_latitude;
+    float utc_time;
+    // RMC - Recommended Minimmum Specific GNS Data
+    int date;
+} GPS_t;
+typedef struct {
+		float co2_ppm;
+		float temperature;
+		float relative_humidity;
+	} CO_t;
+struct sps30_measurement {
+    float mc_2p5;
+    float mc_10p0;
+    float nc_2p5;
+    float nc_10p0;
+    uint16_t SO_ppm;
+};
+typedef struct DATA
+{
+	struct sps30_measurement PM_Data;
+	GPS_t GPS_Data;
+	CO_t CO_Data;
+} CollatedData;
+
+
--- sd/parse.c
+++ sd/parse.c
@@ -6,38 +6,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <dirent.h>
-
-// Maximum range of bytes
-#define MAX 30000
-typedef struct{
-
-
-    // GGA - Global Positioning System Fixed Data
-    float nmea_longitude;
-    float nmea_latitude;
-    float utc_time;
-    // RMC - Recommended Minimmum Specific GNS Data
-    int date;
-} GPS_t;
-typedef struct {
-		float co2_ppm;
-		float temperature;
-		float relative_humidity;
-	} CO_t;
-struct sps30_measurement {
-    float mc_2p5;
-    float mc_10p0;
-    float nc_2p5;
-    float nc_10p0;
-    uint16_t SO_ppm;
-};
-typedef struct DATA
-{
-	struct sps30_measurement PM_Data;
-	GPS_t GPS_Data;
-	CO_t CO_Data;
-} CollatedData;
-
+#include "../common/convert.h"
 void writeCollatedData(FILE* file, const CollatedData* data) {
     
     // Write GPS data
