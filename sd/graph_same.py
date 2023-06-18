993fb00486619ae9f84dbaacc083d0486f919253
unknown <mahikgot@gmail.com>
2023-06-18 18:28:53 +0800

    rearrange graph_same

--- sd/graph_same.py
+++ sd/graph_same.py
@@ -6,7 +6,8 @@ from scipy.signal import medfilt
 from matplotlib.lines import Line2D
 
 # List of CSV filenames
-filenames = ['000000-34321.csv']  # Add more filenames if needed
+filename = '02-03.csv'
+filenames = [filename]  # Add more filenames if needed
 title = 'Air Quality Parameters'
 
 # Read the CSV file
@@ -60,17 +61,19 @@ for i in range(0, len(utc_time)):
 # Divide the data into three segments
 segment_length = len(co2_ppm) // 3
 
+segment1 = np.arange(segment_length)
+segment2 = np.arange(segment_length, 2 * segment_length)
+segment3 = np.arange(2 * segment_length, len(co2_ppm))
 segments = [
-    range(segment_length),
-    range(segment_length, 2 * segment_length),
-    range(2 * segment_length, len(co2_ppm))
+        segment1,
+        segment2,
+        segment3
 ]
 
 variables = [
     temperature_filtered,
     humidity_filtered,
     co2_ppm_filtered,
-    so2_ppm_filtered,
     pm25_ppm_filtered,
 ]
 
@@ -78,16 +81,17 @@ labels = [
     'Temperature (°C)',
     'Relative Humidity (%)',
     'CO2 (ppm)',
-    'SO2 (ppm)',
     'PM2.5 (#/cm^3)',
 ]
 segment_colors = ['blue', 'green', 'orange']  # Colors for each segment
-segment_labels = ['UAV ON, Enclosed', 'UAV OFF, Enclosed', 'UAV OFF, Not Enclosed']  # Custom segment labels
+segment_labels = ['UAV OFF, Enclosed', 'UAV ON, Enclosed', 'UAV ON, Not Enclosed']  # Custom segment labels
 
 # Plot all variables in a single figure with multiple subplots and overlapping segments
 fig, axes = plt.subplots(len(variables), 1, figsize=(10, 15), sharex=True)
 
-fig.suptitle('Experiment 2: Test 1', fontsize=16)
+
+title = 'Experiment 2: Test %d' % (int(filename[4]))
+fig.suptitle(title, fontsize=16)
 
 # Iterate over each variable and its corresponding label
 for ax, variable, label in zip(axes, variables, labels):
@@ -95,7 +99,7 @@ for ax, variable, label in zip(axes, variables, labels):
     handles = []  # List to store the legend handles
     for i, segment in enumerate(segments):
         segment_label = segment_labels[i]
-        line = ax.plot(segment, variable[segment], color=segment_colors[i], label=segment_label)
+        line = ax.plot(np.multiply(segment, 3.5), variable[segment], color=segment_colors[i], label=segment_label)
         handles.append(line[0])
 
         # Calculate the median of the current segment for the variable
@@ -103,10 +107,10 @@ for ax, variable, label in zip(axes, variables, labels):
 
         # Plot the median line starting from the beginning of the segment and ending at the end of the segment
         median_line = ax.axhline(y=segment_median, color=segment_colors[i], linestyle='--',
-                                 xmin=segment.start / len(co2_ppm), xmax=segment.stop / len(co2_ppm))
+                                 xmin=segment[0] / len(co2_ppm), xmax=segment[-1] / len(co2_ppm))
 
         # Calculate the x-coordinate for the annotation
-        x_coord = (segment.start + segment.stop) / 2
+        x_coord = (3.5*segment[0] + 3.5*segment[-1]) / 2
 
         # Annotate the median value near the median line
         ax.annotate(f'{segment_median:.2f}', xy=(x_coord, segment_median),
